8a308d0265bb1af6c116172ea2c3620c8ec009e0
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-10-08 12:38:03 +0600

    some labels added

--- src/components/CreateFund/CreateFundForm.tsx
+++ src/components/CreateFund/CreateFundForm.tsx
@@ -5,7 +5,6 @@ import twitterIcon from "../../img/icons/twitterYellowIcon.svg"
 import discordIcon from "../../img/icons/discordYellowIcon.svg"
 import calendarIcon from "../../img/icons/calendarYellowIcon.svg"
 import parcentageIcon from "../../img/icons/parcentageYellowIcon.svg"
-import { NavLink } from "react-router-dom";
 import Error from "../ui/Error";
 
 const CreateFundForm: FunctionComponent = () => {
@@ -49,36 +48,54 @@ const CreateFundForm: FunctionComponent = () => {
           {
             isError && <Error error={error} />
           }
-          <p className="text-white">Information about your fund</p>
+          <p className="text-white font-bold text-lg">Information about your fund</p>
           <div className="mt-4 space-y-3">
-            <input type="text" name="fundName" id="fundName" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-2.5" placeholder="Your Fund Name" required />
-            <textarea name="about" id="about" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full h-24 p-3" placeholder="About" required></textarea>
-            <textarea name="strategy" id="strategy" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full h-24 p-3" placeholder="Strategy" required></textarea>
+            <div className="space-y-1">
+              <label htmlFor="fundName" className="text-white text-sm">Fund Name</label>
+              <input type="text" name="fundName" id="fundName" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-2.5" placeholder="Your Fund Name" required />
+            </div>
+            <div className="space-y-1">
+              <label htmlFor="about" className="text-white text-sm">About</label>
+              <textarea name="about" id="about" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full h-24 p-3" placeholder="Write somethings about your fund..." required></textarea>
+            </div>
+            <div className="space-y-1">
+              <label htmlFor="strategy" className="text-white text-sm">Strategy</label>
+              <textarea name="strategy" id="strategy" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full h-24 p-3" placeholder="Your fund strategy" required></textarea>
+            </div>
+            
             <div className="flex justify-between space-x-8">
-              <div className="relative w-full">
-                <img className="absolute right-4 top-3" src={telegramIcon} alt="telegram icon" />
+              <div className="relative w-full space-y-1">
+                <img className="absolute right-4 top-10" src={telegramIcon} alt="telegram icon" />
+                <label htmlFor="telegram" className="text-white text-sm">Telegram Username</label>
                 <input onChange={handleTelegram} value={telegram} type="text" name="telegram" id="telegram" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Telegram" required />
               </div>
-              <div className="relative w-full">
-                <img className="absolute right-4 top-3" src={twitterIcon} alt="telegram icon" />
+              <div className="relative w-full space-y-1">
+                <img className="absolute right-4 top-10" src={twitterIcon} alt="twitter icon" />
+                <label htmlFor="twitter" className="text-white text-sm">Twitter Username</label>
                 <input onChange={handleTwitter} value={twitter} type="text" name="twitter" id="twitter" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Twitter" required />
               </div>
-              <div className="relative w-full">
-                <img className="absolute right-4 top-3" src={discordIcon} alt="telegram icon" />
+              <div className="relative w-full space-y-1">
+                <img className="absolute right-4 top-10" src={discordIcon} alt="discord icon" />
+                <label htmlFor="discord" className="text-white text-sm">Discord Username</label>
                 <input onChange={handleDiscord} value={discord} type="text" name="discord" id="discord" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Discord Username" required />
               </div>
             </div>
           </div>
-          <p className="text-white mt-8">Fund Setting</p>
+          <p className="text-white mt-8 font-bold text-lg">Fund Setting</p>
           <div className="mt-4 space-y-3">
-            <input type="number" name="amountRaised" id="amountRaised" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Amounts being raised $" required />
+            <div className="space-y-1">
+              <label htmlFor="amountRaised" className="text-white text-sm">Raising Amount</label>
+              <input type="number" name="amountRaised" id="amountRaised" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Amounts being raised $" required />
+            </div>
             <div className="flex justify-between space-x-8">
-              <div className="relative w-full">
-                <img className="absolute right-4 top-3" src={calendarIcon} alt="telegram icon" />
+              <div className="relative w-full space-y-1">
+                <img className="absolute right-4 top-10" src={calendarIcon} alt="calendar icon" />
+                <label htmlFor="durationOfRaise" className="text-white text-sm">Fund Closing Date <span className="text-xs text-gray-500">(min after 1 months of createing)</span></label>
                 <input type="date" name="durationOfRaise" id="durationOfRaise" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Duration of raise" required />
               </div>
-              <div className="relative w-full">
-                <img className="absolute right-4 top-4" src={parcentageIcon} alt="telegram icon" />
+              <div className="relative w-full space-y-1">
+                <img className="absolute right-4 top-11" src={parcentageIcon} alt="parcentage icon" />
+                <label htmlFor="fees" className="text-white text-sm">Fund Fees</label>
                 <input type="number" name="fees" id="fees" className="bg-transparent border border-gray-600 text-gray-300 text-sm rounded-lg focus:ring-yellow-400 focus:border-yellow-400 block w-full p-3" placeholder="Fees" required />
               </div>
             </div>
