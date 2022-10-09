143455a861071c04951002bdc1369a409ad95207
mahadihassanriyadh <mahadi_hassan3931@yahoo.com>
2022-10-09 18:59:15 +0600

    work in progress

--- src/pages/FundPortfolio/FundPortfolioManage.tsx
+++ src/pages/FundPortfolio/FundPortfolioManage.tsx
@@ -1,9 +1,176 @@
 import React from 'react';
+import { Fragment } from 'react'
+import { Menu, Transition } from '@headlessui/react'
+// import { ChevronDownIcon } from '@heroicons/react/20/solid'
+import dropDownArrow from "../../img/icons/dropDownArrowGray.svg";
+import openSeaLogo from "../../img/fundLogos/openSeaLogo.png";
+import netFineLogo from "../../img/fundLogos/netFineLogo.png";
+import funConLogo from "../../img/fundLogos/funConLogo.png";
+
+
+function classNames(...classes: string[]) {
+  return classes.filter(Boolean).join(' ')
+}
 
 const FundPortfolioManage = () => {
+    const funds = {
+        openSea: {
+            name: "OpenSea Fund",
+            logo: openSeaLogo,
+            assetBalances: [
+                {
+                    asset: "BTC",
+                    balance: 30,
+                },
+                {
+                    asset: "ETH",
+                    balance: 40,
+                },
+            ],
+            protocolBalances: [
+                {
+                    protocol: "Curve",
+                    balance: 29,
+                },
+                {
+                    protocol: "Dopex",
+                    balance: 46,
+                },
+            ],
+        },
+        funCon: {
+            name: "FunCon Fund",
+            logo: funConLogo,
+            assetBalances: [
+                {
+                    asset: "BTC",
+                    balance: 20,
+                },
+                {
+                    asset: "ETH",
+                    balance: 70,
+                },
+            ],
+            protocolBalances: [
+                {
+                    protocol: "Curve",
+                    balance: 12,
+                },
+                {
+                    protocol: "Dopex",
+                    balance: 32,
+                },
+            ],
+        },
+        netFine: {
+            name: "NetFine",
+            logo: netFineLogo,
+            assetBalances: [
+                {
+                    asset: "BTC",
+                    balance: 34,
+                },
+                {
+                    asset: "ETH",
+                    balance: 56,
+                },
+            ],
+            protocolBalances: [
+                {
+                    protocol: "Curve",
+                    balance: 12,
+                },
+                {
+                    protocol: "Dopex",
+                    balance: 82,
+                },
+            ],
+        },
+    }
     return (
-        <div>
-            
+        <div className='container mx-auto my-20'>
+            <Menu as="div" className="relative inline-block text-left">
+                <div>
+                    <Menu.Button className="inline-flex w-full justify-center rounded-md bg-black px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-orange-500 focus:ring-offset-2 focus:ring-offset-orange-100">
+                        <div className='flex justify-between space-x-24'>
+                            <div className='flex space-x-2 items-center'>
+                                <img src={openSeaLogo} alt="" />
+                                <p className='text-2xl font-bold'>OpenSea Fund</p>
+                            </div>
+                            <img src={dropDownArrow} alt="" />
+                        </div>
+                    </Menu.Button>
+                </div>
+
+                <Transition
+                    as={Fragment}
+                    enter="transition ease-out duration-100"
+                    enterFrom="transform opacity-0 scale-95"
+                    enterTo="transform opacity-100 scale-100"
+                    leave="transition ease-in duration-75"
+                    leaveFrom="transform opacity-100 scale-100"
+                    leaveTo="transform opacity-0 scale-95"
+                >
+                    <Menu.Items className="absolute right-0 z-10 mt-2 w-11/12 origin-top-right rounded-md bg-gray-900 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+                    <div className="py-1">
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                            )}
+                            >
+                            Account settings
+                            </a>
+                        )}
+                        </Menu.Item>
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                            )}
+                            >
+                            Support
+                            </a>
+                        )}
+                        </Menu.Item>
+                        <Menu.Item>
+                        {({ active }) => (
+                            <a
+                            href="#"
+                            className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block px-4 py-2 text-sm'
+                            )}
+                            >
+                            License
+                            </a>
+                        )}
+                        </Menu.Item>
+                        <form method="POST" action="#">
+                        <Menu.Item>
+                            {({ active }) => (
+                            <button
+                                type="submit"
+                                className={classNames(
+                                active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+                                'block w-full px-4 py-2 text-left text-sm'
+                                )}
+                            >
+                                Sign out
+                            </button>
+                            )}
+                        </Menu.Item>
+                        </form>
+                    </div>
+                    </Menu.Items>
+                </Transition>
+            </Menu>
         </div>
     );
 };
